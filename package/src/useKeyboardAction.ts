bdfca65f24fac0e3b0c7d50c23d0c12e427a6596
Hamza Khuswan <hamzakhuswan@outlook.com>
2021-07-28 09:14:00 +0200

    🔧 fix target

--- package/src/useKeyboardAction.ts
+++ package/src/useKeyboardAction.ts
@@ -1,54 +1,46 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useEffect, useRef } from "react";
 
-export type KeyboardAction =
-    | {
-          key: string | string[];
-          action: (ev: KeyboardEvent) => void;
-          target?: HTMLElement;
-          condition?: boolean;
-      }
-    | {
-          key: "printable";
-          clearTimeout?: number;
-          action: (ev: KeyboardEvent, keys: string) => void;
-          target?: HTMLElement;
-          condition?: boolean;
-      };
+interface KeyboardAction {
+    key: "printable" | string | string[];
+    action: (ev: KeyboardEvent, keys?: string) => void;
+    clearTimeout?: number;
+    target?: HTMLElement | Window | null;
+    condition?: boolean;
+}
 
 export function useKeyboardAction(keyboardAction: KeyboardAction): void {
     const clearPrintableTimeoutRef = useRef<ReturnType<typeof setTimeout>>();
     const printableRef = useRef("");
 
     useEffect(() => {
-        const EventTarget = keyboardAction?.target || window;
+        const EventTarget = keyboardAction?.target;
         const condition = keyboardAction.condition ?? true;
-        if (!condition) return;
+        if (!condition || !EventTarget) return;
 
         let handler: ((ev: KeyboardEvent) => void) | null = null;
-        if (typeof keyboardAction.key === "string") {
-            if (keyboardAction.key === "printable") {
-                handler = (ev: KeyboardEvent) => {
-                    if (ev.key.length === 1 && !ev.ctrlKey) {
-                        ev.preventDefault();
-                        if (clearPrintableTimeoutRef.current) clearTimeout(clearPrintableTimeoutRef.current);
-                        clearPrintableTimeoutRef.current = setTimeout(() => {
-                            printableRef.current = "";
-                        }, (keyboardAction as any).clearTimeout || 750);
-                        printableRef.current += ev.key;
+        if (keyboardAction.key === "printable") {
+            handler = (ev: KeyboardEvent) => {
+                if (ev.key.length === 1 && !ev.ctrlKey) {
+                    ev.preventDefault();
+                    if (clearPrintableTimeoutRef.current) clearTimeout(clearPrintableTimeoutRef.current);
+                    clearPrintableTimeoutRef.current = setTimeout(() => {
+                        printableRef.current = "";
+                    }, (keyboardAction as any).clearTimeout || 750);
+                    printableRef.current += ev.key;
 
-                        keyboardAction.action(ev, printableRef.current);
-                    }
-                };
-            } else {
-                handler = (ev: KeyboardEvent) => {
-                    if (ev.code === keyboardAction.key) {
-                        ev.preventDefault();
-                        keyboardAction.action(ev, "");
-                    }
-                };
-            }
-        } else if (Array.isArray(keyboardAction.key)) {
+                    keyboardAction.action(ev, printableRef.current);
+                }
+            };
+        } else if (typeof keyboardAction.key === "string") {
+            handler = (ev: KeyboardEvent) => {
+                if (ev.code === keyboardAction.key) {
+                    ev.preventDefault();
+                    keyboardAction.action(ev);
+                }
+            };
+        }
+        else if (Array.isArray(keyboardAction.key)) {
             handler = (ev: KeyboardEvent) => {
                 if ((keyboardAction.key as string[]).some((key) => key === ev.code)) {
                     keyboardAction.action(ev, "");
@@ -56,11 +48,10 @@ export function useKeyboardAction(keyboardAction: KeyboardAction): void {
             };
         }
 
-        if (handler) {
-            (EventTarget as HTMLElement).addEventListener("keydown", handler);
-            return () => {
-                (EventTarget as HTMLElement).removeEventListener("keydown", handler as () => void);
-            };
-        }
+        if (handler) (EventTarget as HTMLElement).addEventListener("keydown", handler);
+        return () => {
+            if (handler) (EventTarget as HTMLElement).removeEventListener("keydown", handler);
+        };
+
     }, [keyboardAction]);
 }
